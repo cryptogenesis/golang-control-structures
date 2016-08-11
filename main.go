c1f232754e28b9d1810d453255fe6ea8a9a6fa7e
EVAN ROSE <evan@cryptogenesis.com>
2016-08-11 18:08:59 -0500

    update

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
+
 import "fmt"
+
 var (
-author = "Evan Rose"
-Version = "0.0.1"
+	author  = "Evan Rose"
+	Version = "0.0.1"
 )
 
 func main() {
+	if true == false {
+		fmt.Println("true is true")
+	} else {
+		fmt.Println("true is false")
+	}
 
+	for i := 0; i < 20; i++ {
+		fmt.Println(i)
+	}
+
+	for {
+		fmt.Println("This will go forever.")
+		break
+	}
+
+	j := true
+	z := 0
+
+	for j == true {
+		fmt.Println("J is true.")
+		z++
+		if z == 2 {
+			j = false
+			fmt.Println("J is now false - break")
+		} else {
+			fmt.Println(z)
+		}
+	}
 }
